a1dd635c54868475f410e88111348ea97169cc7c
Jauadul15 <rashik.gmh@gmail.com>
2023-01-02 19:59:00 +0600

    Create jsonresponse.js
    create json response

---
+++ jsonresponse.js
@@ -0,0 +1,21 @@
+//json response 
+var express=require('express');
+
+var app=express();
+
+app.get('/',function (req,res) {
+    var mydata=[
+        {
+            name:"jauadul karim",
+            city:'ctg',
+            id:'019'
+        },
+        {
+            name:"minhazul karim",
+            city:'ctg',
+            id:'016'
+        }
+    ]
+    res.json(mydata)
+})
+app.listen(8000);
